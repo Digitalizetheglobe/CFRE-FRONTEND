e02b4244acce95d14defb310a3ca4a0c7a914b75
Gajendra-28 <147844763+Gajendra-28@users.noreply.github.com>
2024-08-20 11:07:25 +0530

    add property form

--- src/AdminDashboard/PropertyPages/AddProperty.jsx
+++ src/AdminDashboard/PropertyPages/AddProperty.jsx
@@ -157,7 +157,6 @@ const AddProperty = () => {
           <label className="block text-sm font-medium text-gray-700" htmlFor="agreementCharges">Agreement Charges</label>
           <input type="text" name="agreementCharges" id="agreementCharges" className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2" value={formData.agreementCharges} onChange={handleChange} required />
         </div>
-
         <div className="mb-4">
           <label className="block text-sm font-medium text-gray-700" htmlFor="bannerImage">Banner Image</label>
           <input type="file" name="bannerImage" id="bannerImage" className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2" onChange={handleFileChange} />
